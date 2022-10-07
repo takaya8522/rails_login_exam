d3f74c065b08edbd1f453321fe46e913c5ea361f
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-07 13:17:06 +0900

    users・sessionsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks, only: [:index, :create, :new, :edit, :show, :update, :destroy]
+  resources :session, only: [:new]
+  resources :users, only: [:new, :edit, :show, :update, :destroy]
 end
