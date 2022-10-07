9c6f42baeed00a14368a0da24fd9ff04cc8c25df
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-07 21:48:01 +0900

    修正1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks, only: [:index, :create, :new, :edit, :show, :update, :destroy]
-  resources :session, only: [:new]
-  resources :users, only: [:new, :edit, :show, :update, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :edit, :show, :update, :destroy]
 end
